3a68a7802217cddc3f7e7462921db268d64d4e4d
wangwenche <wangwenche1@outlook.com>
2025-03-24 09:19:11 +0800

    Commit a test case and get the expected failure.

--- functional_tests.py
+++ functional_tests.py
@@ -1,6 +1,18 @@
 from selenium import webdriver
+import unittest
 
-browser = webdriver.Chrome()
-browser.get('http://localhost:8000')
-assert 'Django' in browser.page_source
-browser.quit()
+class NewVisitorTest(unittest.TestCase):
+    
+    def setUp(self):
+        self.browser = webdriver.Chrome()
+    
+    def tearDown(self):
+        self.browser.quit()
+        
+    def test_can_start_a_list_and_retrieve_it_later(self):
+        self.browser.get('http://localhost:8000')
+        self.assertIn('To-Do', self.browser.title), "Browser title was " + self.browser.title
+        self.fail("Finish the test!")
+        
+if __name__ == '__main__':
+    unittest.main()
