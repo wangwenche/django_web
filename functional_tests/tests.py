04560c30172533543f2fb95d095fa51436b6b468
wangwenche <wangwenche1@outlook.com>
2025-04-28 10:26:03 +0800

    message 14: add a new user

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -69,7 +69,49 @@ class NewVisitorTest(LiveServerTestCase):
         
         # 张三想知道这个网站是否会记住她的清单
         # 他看到网站为他生成了一个唯一的URL
-        self.fail("Finish the test!")
+        # self.fail("Finish the test!")
         
         # 页面再次更新，她的清单中显示了这两个待办事项
         
+    def test_multiple_users_can_start_lists_at_different_urls(self):
+        # 张三新建一个待办事项清单
+        self.browser.get(self.live_server_url)
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        inputbox.send_keys('Buy flowers')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy flowers')
+        
+        # 他注意到清单有个唯一的URL
+        zhangsan_list_url = self.browser.current_url
+        self.assertRegex(zhangsan_list_url, '/lists/.+')
+        
+        # 现在一个新用户王五访问网站
+        # 我们使用一个新的浏览器会话
+        # 确保张三的信息不会从cookie中泄露出去
+        self.browser.quit()
+        self.browser = webdriver.Chrome()
+        
+        # 王五访问首页
+        # 页面中看不到张三的清单
+        self.browser.get(self.live_server_url)
+        page_text = self.browser.find_element(By.TAG_NAME, 'body').text
+        self.assertNotIn('Buy flowers', page_text)
+        self.assertNotIn('Give a gift to Lisi', page_text)
+        
+        # 王五输入一个新待办事项，新建一个清单
+        inputbox = self.browser.find_element(By.ID, 'id_new_item')
+        inputbox.send_keys('Buy milk')
+        inputbox.send_keys(Keys.ENTER)
+        self.wait_for_row_in_list_table('1: Buy milk')
+        
+        # 王五获得了他的唯一URL
+        wangwu_list_url = self.browser.current_url
+        self.assertRegex(wangwu_list_url, '/lists/.+')
+        self.assertNotEqual(wangwu_list_url, zhangsan_list_url)
+        
+        # 这个页面还是没有张三的清单
+        page_text = self.browser.find_element(By.TAG_NAME, 'body').text
+        self.assertNotIn('Buy flowers', page_text)
+        self.assertIn('Buy milk', page_text)
+        
+        # 两个人都很满意，然后去睡觉了
