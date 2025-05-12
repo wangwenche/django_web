5b482787f405113b5dad31f34c02a10b2880dac7
wangwenche <wangwenche1@outlook.com>
2025-05-12 10:36:31 +0800

    message 24: move static file and database outside of main source tree

--- functional_tests/tests.py
+++ functional_tests/tests.py
@@ -4,6 +4,7 @@ import time
 from selenium.webdriver.common.by import By
 from django.contrib.staticfiles.testing import StaticLiveServerTestCase
 from selenium.common.exceptions import WebDriverException
+import os
 
 MAX_WAIT = 10
 
@@ -15,7 +16,10 @@ class NewVisitorTest(StaticLiveServerTestCase):
     
     def setUp(self):
         self.browser = webdriver.Chrome()
-    
+        real_server = os.environ.get('REAL_SERVER')
+        if real_server:
+            self.live_server_url = 'http://' + real_server
+        
     def tearDown(self):
         self.browser.quit()
         
